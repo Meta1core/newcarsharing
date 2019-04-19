8113bee6bcaad9b82af5d0989b4dbd7085ac03e8
Annihilator <howk2000@mail.ru>
2019-04-19 12:19:50 +0300

    Добавлен файл gitignore, убраны ненужные строки в restcontroll

--- src/main/java/com/exposit/carsharing/web/UserControllerv1.java
+++ src/main/java/com/exposit/carsharing/web/UserControllerv1.java
@@ -9,14 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.jws.soap.SOAPBinding;
 import javax.validation.Valid;
 
 
@@ -31,10 +28,6 @@ public class UserControllerv1{
     private UserValidator userValidator;
 
 
-    private UserLoginPayload userLoginPayload;
-
-
-
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public ResponseEntity <User>  registration(@RequestBody @Valid User user, BindingResult bindingResult) {
         userValidator.validate(user, bindingResult);
@@ -47,11 +40,11 @@ public class UserControllerv1{
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity <UserLoginPayload> login(@RequestBody UserLoginPayload userLoginPayload) {
+    public ResponseEntity <User> login(@RequestBody UserLoginPayload userLoginPayload) {
         String username = userLoginPayload.getUsername();
         String password = userLoginPayload.getPassword();
 
-                return new ResponseEntity<>( HttpStatus.OK);
+                return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
