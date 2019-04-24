b599bff29b81a0cb37a140a194261b0e733acf13
Annihilator <howk2000@mail.ru>
2019-04-24 16:45:14 +0300

    исправления

--- src/main/java/com/exposit/carsharing/service/impl/CarServiceImpl.java
+++ src/main/java/com/exposit/carsharing/service/impl/CarServiceImpl.java
@@ -6,7 +6,6 @@ import com.exposit.carsharing.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Slf4j
