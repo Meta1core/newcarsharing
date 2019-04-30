40534ca1201bf1c0728448e809328d88dc7dcbbe
Annihilator <howk2000@mail.ru>
2019-04-30 14:34:23 +0300

    Исправлен метод signin

--- src/main/java/com/exposit/carsharing/service/impl/UserServiceImpl.java
+++ src/main/java/com/exposit/carsharing/service/impl/UserServiceImpl.java
@@ -10,13 +10,15 @@ import com.exposit.carsharing.security.config.JwtTokenProvider;
 import com.exposit.carsharing.service.UserService;
 import lombok.AllArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.AuthenticationException;
-
+@Slf4j
 @AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
@@ -35,9 +37,13 @@ public class UserServiceImpl implements UserService {
 
     public String signin(String username, String password) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+           User dbpassword =  userRepository.findByUsername(username);
+            if (!bCryptPasswordEncoder.matches(password, dbpassword.getPassword())) {
+                log.error("Incorrect password for user with email={0} and ID={1}",
+                        dbpassword.getEmail(), dbpassword.getId());
+            }
             return jwtTokenProvider.createToken(username, userRepository.findByUsername(username).getRoles());
-        } catch (AuthenticationException e) {
+        }   catch (AuthenticationException e) {
             throw new CustomException("Invalid username/password supplied", HttpStatus.UNPROCESSABLE_ENTITY);
         }
     }
